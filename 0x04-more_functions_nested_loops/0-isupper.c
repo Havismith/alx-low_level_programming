3066dab1aab6618364624614abf6ff6aca5520f1
Havismith <chukwuebukaeze1000@gmail.com>
2023-02-24 07:45:30 -0800

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - checks for uppercase charaecter
  *
@@ -7,13 +6,11 @@
  *
  * Return: returns 1 if it is uppercase and 0 if it is not
  */
-
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return (1);
+	return (1);
 	}
-	else
-		return (0);
+	return (0);
 }
